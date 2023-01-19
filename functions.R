3e62cce6010ae639015c8981e3c45c75bcec015e
Marina Vabistsevits <ny19205@bristol.ac.uk>
2023-01-19 17:48:34 +0000

    tidy up plotting

--- functions.R
+++ functions.R
@@ -19,6 +19,218 @@ tidy_pvals<-function(df){
 }
 
 
+mr_scatter_plot_manual <- function (mr_results, dat) 
+{
+  requireNamespace("ggplot2", quietly = TRUE)
+  requireNamespace("plyr", quietly = TRUE)
+  mrres <- plyr::dlply(dat, c("id.exposure", "id.outcome"), 
+                       function(d) {
+                         d <- plyr::mutate(d)
+                         if (nrow(d) < 2 | sum(d$mr_keep) == 0) {
+                           return(blank_plot("Insufficient number of SNPs"))
+                         }
+                         d <- subset(d, mr_keep)
+                         index <- d$beta.exposure < 0
+                         d$beta.exposure[index] <- d$beta.exposure[index] * 
+                           -1
+                         d$beta.outcome[index] <- d$beta.outcome[index] * 
+                           -1
+                         mrres <- subset(mr_results, id.exposure == d$id.exposure[1] & 
+                                           id.outcome == d$id.outcome[1])
+                         mrres$a <- 0
+                         if ("MR Egger" %in% mrres$method) {
+                           temp <- mr_egger_regression(d$beta.exposure, 
+                                                       d$beta.outcome, d$se.exposure, d$se.outcome, 
+                                                       default_parameters())
+                           mrres$a[mrres$method == "MR Egger"] <- temp$b_i
+                         }
+                         if ("MR Egger (bootstrap)" %in% mrres$method) {
+                           temp <- mr_egger_regression_bootstrap(d$beta.exposure, 
+                                                                 d$beta.outcome, d$se.exposure, d$se.outcome, 
+                                                                 default_parameters())
+                           mrres$a[mrres$method == "MR Egger (bootstrap)"] <- temp$b_i
+                         }
+                         ggplot2::ggplot(data = d, ggplot2::aes(x = beta.exposure, 
+                                                                y = beta.outcome)) +
+                           ggplot2::geom_errorbar(ggplot2::aes(ymin = beta.outcome -  se.outcome, 
+                                                               ymax = beta.outcome + se.outcome), 
+                                                               colour = "grey", width = 0) +
+                           ggplot2::geom_errorbarh(ggplot2::aes(xmin = beta.exposure -  se.exposure,
+                                                                xmax = beta.exposure + se.exposure), colour = "grey", height = 0) +
+                           ggplot2::geom_point() + 
+                           ggplot2::geom_abline(data = mrres, ggplot2::aes(intercept = a, 
+                                                                           slope = b, colour = method), show.legend = TRUE) + 
+                           ggplot2::scale_colour_manual(values = c("#a6cee3", 
+                                                                   "#1f78b4", "#b2df8a", "#33a02c", "#fb9a99", 
+                                                                   "#e31a1c", "#fdbf6f", "#ff7f00", "#cab2d6", 
+                                                                   "#6a3d9a", "#ffff99", "#b15928")) +
+                           ggplot2::labs(colour = "MR Test", 
+                                          x = paste("SNP effect on", d$exposure[1]),
+                                          y = paste("SNP effect on",  d$outcome[1])) + 
+                           
+                           ggplot2::theme(legend.position = "top",   legend.direction = "vertical") + 
+                          
+                           
+                           theme_bw() + 
+                           ggplot2::guides(colour = ggplot2::guide_legend(ncol = 1))
+                       })
+  mrres
+}
+
+
+mr_leaveoneout_plot_manual <- function (leaveoneout_results) 
+{
+  requireNamespace("ggplot2", quietly = TRUE)
+  requireNamespace("plyr", quietly = TRUE)
+  res <- plyr::dlply(leaveoneout_results, c("id.exposure", 
+                                            "id.outcome"), function(d) {
+                                              d <- plyr::mutate(d)
+                                              if (sum(!grepl("All", d$SNP)) < 3) {
+                                                return(blank_plot("Insufficient number of SNPs"))
+                                              }
+                                              d$up <- d$b + 1.96 * d$se
+                                              d$lo <- d$b - 1.96 * d$se
+                                              d$tot <- 1
+                                              d$tot[d$SNP != "All"] <- 0.01
+                                              d$SNP <- as.character(d$SNP)
+                                              nom <- d$SNP[d$SNP != "All"]
+                                              nom <- nom[order(d$b)]
+                                              d <- rbind(d, d[nrow(d), ])
+                                              d$SNP[nrow(d) - 1] <- ""
+                                              d$b[nrow(d) - 1] <- NA
+                                              d$up[nrow(d) - 1] <- NA
+                                              d$lo[nrow(d) - 1] <- NA
+                                              d$SNP <- ordered(d$SNP, levels = c("All", "", nom))
+                                              ggplot2::ggplot(d, ggplot2::aes(y = SNP, x = b)) +
+                                                ggplot2::geom_vline(xintercept = 0,  linetype = "dotted") +
+                                                ggplot2::geom_errorbarh(ggplot2::aes(xmin = lo,  xmax = up, 
+                                                                                     size = as.factor(tot), 
+                                                                                     colour = as.factor(tot)),
+                                                                        height = 0) +
+                                                ggplot2::geom_point(ggplot2::aes(colour = as.factor(tot))) + 
+                                                ggplot2::geom_hline(ggplot2::aes(yintercept = which(levels(SNP) %in%  "")), colour = "grey") + 
+                                                
+                                                ggplot2::scale_colour_manual(values = c("black",  "red")) + 
+                                                ggplot2::scale_size_manual(values = c(0.3,   1)) +
+                                                theme_bw()+
+                                                ggplot2::theme(legend.position = "none", 
+                                                               axis.text.y = ggplot2::element_text(size = 8), 
+                                                               axis.ticks.y = ggplot2::element_line(size = 0), 
+                                                               axis.title.x = ggplot2::element_text(size = 8)) + 
+                                                ggplot2::labs(y = "", x = paste0("MR leave-one-out sensitivity analysis for\n'", 
+                                                                                 d$exposure[1], "' on '", d$outcome[1], "'"))
+                                            })
+  res
+}
+
+
+
+mr_forest_plot_outliers <- function (d,  outliers_list, outliers_colour, method) {
+    # this is a modified version of 2SMR function 
+
+     requireNamespace("ggplot2", quietly = TRUE)
+     requireNamespace("plyr", quietly = TRUE)
+
+     if (sum(!grepl("All", d$SNP)) < 2) {
+       return(blank_plot("Insufficient number of SNPs"))}
+
+     levels(d$SNP)[levels(d$SNP) == "All - Inverse variance weighted"] <- "All - IVW"
+     levels(d$SNP)[levels(d$SNP) == "All - MR Egger"] <- "All - Egger"
+     am <- grep("All", d$SNP, value = TRUE)
+     d$up <- d$b + 1.96 * d$se
+     d$lo <- d$b - 1.96 * d$se
+     d$tot <- 0.01
+     d$tot[d$SNP %in% am] <- 1
+     d$SNP <- as.character(d$SNP)
+     nom <- d$SNP[!d$SNP %in% am]
+     nom <- nom[order(d$b)]
+     d <- rbind(d, d[nrow(d), ])
+     d$SNP[nrow(d) - 1] <- ""
+     d$b[nrow(d) - 1] <- NA
+     d$up[nrow(d) - 1] <- NA
+     d$lo[nrow(d) - 1] <- NA
+     d$SNP <- ordered(d$SNP, levels = c(am, "", nom))
+     xint <- 0
+
+     
+     d <- d %>% mutate(outlier = ifelse(SNP %in% outliers_list, T, F))
+     
+     out <- ggplot2::ggplot(d, ggplot2::aes(y = SNP, x = b)) + 
+       ggplot2::geom_vline(xintercept = xint, linetype = "dotted") + 
+       ggplot2::geom_errorbarh(ggplot2::aes(xmin = lo, 
+                                            xmax = up, size = as.factor(outlier), colour = as.factor(outlier)), 
+                               height = 0) + ggplot2::geom_point(ggplot2::aes(colour = as.factor(outlier))) + 
+       ggplot2::geom_hline(ggplot2::aes(yintercept = which(levels(SNP) %in% 
+                                                             "")), colour = "grey") + 
+       ggplot2::scale_colour_manual(values = c('FALSE' = "black",  'TRUE' = outliers_colour)) +
+       ggplot2::scale_size_manual(values = c(0.3,  1)) + 
+       ggplot2::theme_bw()+
+       ggplot2::theme(legend.position = "none", 
+                      axis.text.y = ggplot2::element_text(size = 8), 
+                      axis.ticks.y = ggplot2::element_line(size = 0), 
+                      axis.title.x = ggplot2::element_text(size = 8)) + 
+       ggplot2::labs(y = "", 
+                     subtitle = paste0("Single SNP forest plot with ",method," outliers"),
+                     x = paste0("MR effect size for\n'", 
+                                        d$exposure[1], "' on '", d$outcome[1], "'"))
+     return(out)
+   
+  
+}
+
+mr_forest_plot_clusters <- function (d,  outliers_df, outliers_colour_list) {
+  # this is a modified version of 2SMR function 
+  
+  requireNamespace("ggplot2", quietly = TRUE)
+  requireNamespace("plyr", quietly = TRUE)
+  
+  if (sum(!grepl("All", d$SNP)) < 2) {
+    return(blank_plot("Insufficient number of SNPs"))}
+  
+  levels(d$SNP)[levels(d$SNP) == "All - Inverse variance weighted"] <- "All - IVW"
+  levels(d$SNP)[levels(d$SNP) == "All - MR Egger"] <- "All - Egger"
+  am <- grep("All", d$SNP, value = TRUE)
+  d$up <- d$b + 1.96 * d$se
+  d$lo <- d$b - 1.96 * d$se
+  d$tot <- 0.01
+  d$tot[d$SNP %in% am] <- 1
+  d$SNP <- as.character(d$SNP)
+  nom <- d$SNP[!d$SNP %in% am]
+  nom <- nom[order(d$b)]
+  d <- rbind(d, d[nrow(d), ])
+  d$SNP[nrow(d) - 1] <- ""
+  d$b[nrow(d) - 1] <- NA
+  d$up[nrow(d) - 1] <- NA
+  d$lo[nrow(d) - 1] <- NA
+  
+  d <- d %>% left_join(outliers_df, by= c("SNP"="rsID")) 
+  
+  d$SNP <- ordered(d$SNP, levels = c(am, "", nom))
+  xint <- 0
+  
+
+  
+  out <- ggplot2::ggplot(d, ggplot2::aes(y = SNP, x = b)) + 
+    ggplot2::geom_vline(xintercept = xint, linetype = "dotted") + 
+    ggplot2::geom_errorbarh(ggplot2::aes(xmin = lo, xmax = up, colour = as.factor(cluster)), height = 0) +
+    ggplot2::geom_point(ggplot2::aes(colour = as.factor(cluster))) + 
+    ggplot2::geom_hline(ggplot2::aes(yintercept = which(levels(SNP) %in% "")), colour = "black") + 
+    ggplot2::scale_colour_manual(values = outliers_colour_list) +
+    ggplot2::theme_bw()+
+    ggplot2::theme(legend.position = "none", 
+                   axis.text.y = ggplot2::element_text(size = 8), 
+                   axis.ticks.y = ggplot2::element_line(size = 0), 
+                   axis.title.x = ggplot2::element_text(size = 8)) + 
+    ggplot2::labs(y = "", 
+                  subtitle = "Single SNP forest plot with MR-Clust clusters",
+                  x = paste0("MR effect size for\n'", 
+                                     d$exposure[1], "' on '", d$outcome[1], "'"))
+  return(out)
+  
+  
+}
+
+
 calc_steiger <- function(harmonised, exposure_ss, outcome_ss, outcome_ncase = NA, outcome_ncontrol =NA){
   # assumed both traits are continuous, not binary
   
